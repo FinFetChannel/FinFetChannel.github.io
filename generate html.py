3856b2d2ab8758a0453d65dfa103304f37c39ac6
FinFET <76776190+FinFetChannel@users.noreply.github.com>
2023-02-19 08:44:14 -0300

    Rearranging

--- generate html.py
+++ generate html.py
@@ -21,23 +21,47 @@ def main():
                     with tag('a', href='https://www.youtube.com/@FinFET'):
                         text('channel on YouTube') 
                     text(' where I try stuff out with programming (especially Python) and tech in general.')
-            
+                social_media(tag)
             with tag('main'):
                 with tag('aside'):pass
 
                 with tag('article'):
+
                     ###############################################################################
-                    with tag('h2'):
-                        text('Simple Raycasting with Matplotlib')
+                    with tag('h2'): text("2022/2023 - Super MaRayO Caster")
                     with tag('p'):
-                        text('''One of my first projects in Python, a simple raycasting game with matplotlib, drawing walls as
-                            vertical lines on a grid system.''')    
-                    video_frame(tag, '5xyeWBxmqzc')
+                        text('''A first-person perspective version of Super Mario Bros. using raycasting, made in Python with PyGame and PygBag.''')
+                    video_frame(tag, 'NXhRi8UgzZk')
+                    doc.stag('br')
+                    game_frame(tag, text, '7339954')
+                    itch_frame(tag, '1789891')
                     end_post(doc)
 
                     ###############################################################################
-                    with tag('h2'):
-                        text('Pytracing Maze - my first game release')
+                    with tag('h2'): text("2022 - The LockPickin'joyer")
+                    with tag('p'):
+                        text('''A game inspired by the LockPickingLawyer, made in Python with PyGame and PygBag.''')
+                    video_frame(tag, 'p-QJxbZmP3c')
+                    doc.stag('br')
+                    game_frame(tag, text, '6464625')
+                    itch_frame(tag, '1474850')
+                    end_post(doc)
+
+                    ###############################################################################
+                    with tag('h2'): text('2021/2022 - Dead And!')
+                    with tag('p'): text('''A raycasting game made in Python, now with textures and sprites.''')
+                    video_frame(tag, 'FLc6vUwyTdM')
+                    with tag('p'):
+                        text('Themes developed include:')
+                        with tag('ul'):
+                            for item in ['Floorcasting', 'Raycasting with textures and fake shadows', 'Sprites',
+                                         'Enemies AI', 'Sounds', 'Simple GUI and menus', 'Level design']:
+                                line('li',item)
+                    itch_frame(tag, '1326223')
+                    end_post(doc)
+                    
+                    ###############################################################################
+                    with tag('h2'): text('2021 - Pytracing Maze - my first game release')
                     with tag('p'):
                         text('''A simple 3D maze game, where you have to find the exit to pass to the next level, 
                         but beware of enemies.  The final objective is to obtain the highest score, you 
@@ -54,48 +78,19 @@ def main():
                     end_post(doc)
 
                     ###############################################################################
-                    with tag('h2'): text('Dead And!')
-                    with tag('p'): text('''A raycasting game made in Python, now with textures and sprites.''')
-                    video_frame(tag, 'FLc6vUwyTdM')
+                    with tag('h2'): text('2020 - Simple Raycasting with Matplotlib')
                     with tag('p'):
-                        text('''Themes developed include:''')
-                        with tag('ul'):
-                            for item in ['Floorcasting', 'Raycasting with textures and fake shadows', 'Sprites',
-                                         'Enemies AI', 'Sounds', 'Simple GUI and menus', 'Level design']:
-                                line('li',item)
-                    itch_frame(tag, '1326223')
-                    end_post(doc)
-                    
-                    ###############################################################################
-                    with tag('h2'):
-                        text("The LockPickin'joyer")
-                    with tag('p'):
-                        text('''A game inspired by the LockPickingLawyer, made in Python with PyGame and PygBag.''')
-                    video_frame(tag, 'p-QJxbZmP3c')
-                    doc.stag('br')
-                    game_frame(tag, text, '6464625')
-                    itch_frame(tag, '1474850')
-                    end_post(doc)
-
-                    ###############################################################################
-                    with tag('h2'):
-                        text("Super MaRayO Caster")
-                    with tag('p'):
-                        text('''A first-person perspective version of Super Mario Bros. using raycasting, made in Python with PyGame and PygBag.''')
-                    video_frame(tag, 'NXhRi8UgzZk')
-                    doc.stag('br')
-                    game_frame(tag, text, '7339954')
-                    itch_frame(tag, '1789891')
+                        text('''One of my first projects in Python, a simple raycasting game with matplotlib, drawing walls as
+                            vertical lines on a grid system.''')    
+                    video_frame(tag, '5xyeWBxmqzc')
                     end_post(doc)
 
                 with tag('aside'): pass
 
             with tag('footer'):
-                with tag('a', href="https://www.reddit.com/r/FinFET/", klass="fab fa-reddit fa-2x"):pass
-                with tag('a', href="https://finfetchannel.itch.io/", klass="fab fa-itch-io fa-2x"):pass
-                with tag('a', href="https://www.youtube.com/@FinFET", klass="fab fa-youtube fa-2x"):pass
-                with tag('a', href="https://github.com/FinFetChannel", klass="fab fa-github fa-2x"):pass
-                with tag('a', href="https://discord.com/invite/ZQmnZc4ARa", klass="fab fa-discord fa-2x"):pass
+                social_media(tag)
+                with tag('p'):
+                     text('2023 FinFET Channel. All rights reserved.')
             
     with open("index.html", "w") as f:
         f.writelines(indent(doc.getvalue()))
@@ -123,5 +118,13 @@ def game_frame(tag, text, game):
 def end_post(doc):
     doc.stag('br'); doc.stag('hr'); doc.stag('br')
 
+def social_media(tag):
+    with tag('a', href="https://www.reddit.com/r/FinFET/", klass="fab fa-reddit fa-2x"):pass
+    with tag('a', href="https://finfetchannel.itch.io/", klass="fab fa-itch-io fa-2x"):pass
+    with tag('a', href="https://www.youtube.com/@FinFET", klass="fab fa-youtube fa-2x"):pass
+    with tag('a', href="https://github.com/FinFetChannel", klass="fab fa-github fa-2x"):pass
+    with tag('a', href="mailto:finfetchannel@outlook.com", klass="fa fa-envelope fa-2x"):pass
+    with tag('a', href="https://discord.com/invite/ZQmnZc4ARa", klass="fab fa-discord fa-2x"):pass
+    
 if __name__ == '__main__':
     main()
